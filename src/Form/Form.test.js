80076bce7f2ff2395aa22510a073db4660543564
Shawn Truesdale <shawnctruesdale@gmail.com>
2020-12-16 11:26:36 -0700

    Add type input testing to Form.test

--- src/Form/Form.test.js
+++ src/Form/Form.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { screen, render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import '@testing-library/jest-dom';
 import Form from './Form.js';
 
@@ -11,5 +12,13 @@ describe('Form', () => {
     expect(screen.getByPlaceholderText('Date (mm/dd)')).toBeInTheDocument();
     expect(screen.getByPlaceholderText('Time')).toBeInTheDocument();
     expect(screen.getByPlaceholderText('Number of Guests')).toBeInTheDocument();
+  });
+
+  it('should reflect the users typed value inside the input field', () => {
+    render(<Form />);
+
+    const nameInput = screen.getByPlaceholderText('Name');
+    userEvent.type(nameInput, 'Spud McButts');
+    expect(nameInput).toHaveValue('Spud McButts');
   });
 });
